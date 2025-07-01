05ca3bcf2cb86cf257cbc07fbc0ec2075306bc1d
ImaStudy <kubatchuitenov254@gmail.com>
2025-07-01 20:29:09 +0600

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_ahS1_vqjYA6fpzM0tTBZx-oBkL3X2Jo",
+  authDomain: "imastudy-631ec.firebaseapp.com",
+  projectId: "imastudy-631ec",
+  storageBucket: "imastudy-631ec.firebasestorage.app",
+  messagingSenderId: "301505972939",
+  appId: "1:301505972939:web:061de2a898044090dae94e",
+  measurementId: "G-305V13PQ4Q"
+};
